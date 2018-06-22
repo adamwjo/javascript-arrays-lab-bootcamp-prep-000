2210cf16f5bc0d33dc1389b7b9c28b92f9a344dd
Adam Johnson <adamwjo@gmail.com>
2018-06-22 18:06:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(kitten) {
+  return kittens.push(kitten)
+}
+
+function destructivelyPrependKitten(kitten) {
+  return kittens.unshift(kitten)
+
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+
+}
